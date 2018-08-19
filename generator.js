ce3d5c662f671835854f96c6c2f2d75ad2625972
Taro <15029352778@163.com>
2018-08-20 00:12:34 +0800

    add api update

--- generator.js
+++ generator.js
@@ -1,13 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+
 module.exports = (api, options, rootOptions) => {
   // 修改 `package.json` 里的字段
   api.extendPackage({
     scripts: {
-      'build:api': 'vue-cli-service nei-api-get'
+      'build:api': 'vue-cli-service nei-api-get',
+      'update:api': 'vue-cli-service nei-api-update',
     }
   })
 
   // 配置 .gitignore 文件, 忽略 .yuntaiconfig 文件
-  
+  const gitignorePath = path.resolve('.', '.gitignore')
+  let content = '.yuntaiconfig'
+  if (fs.existsSync(gitignorePath)) {
+    fs.appendFileSync(gitignorePath, content)
+  } else {
+    fs.writeFileSync(gitignorePath, content)
+  }
 
   if (options.S_INFO && options.KOA_SID && options.KOA_SID_SIG) {
     api.render('./template', options)
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const neiApiGet = require('./service/neiApiGet')
+const neiApiUpdate = require('./service/neiApiUpdate')
 
 module.exports = (api, projectOptions) => {
   api.registerCommand('nei-api-get', args => {
     neiApiGet(api, projectOptions, args)
+  })
+
+  api.registerCommand('nei-api-update', args => {
+    neiApiUpdate(api, projectOptions, args)
   })
 }
